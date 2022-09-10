27a1f68d926d178fb9e985d1ad57b86e555453fe
catherinekimani <catherinekimani882@gmail.com>
2022-09-11 01:01:05 +0300

    Ft: Create contact us component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutComponent } from './aboutUs/about/about.component';
+import { ContactComponent } from './contactUs/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LandingPageComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
