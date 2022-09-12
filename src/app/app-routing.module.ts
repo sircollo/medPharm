333be411691df5fff2c39ed9e2b0e191510cc83f
Collins Sirwani <sircollo71@gmail.com>
2022-09-13 02:58:46 +0300

    Ft: add patient-login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
+import { ContactComponent } from './contactUs/contact/contact.component';
+import { AboutComponent } from './aboutUs/about/about.component';
+import { NurseComponent } from './nurse/nurse.component';
+import { PatientLoginComponent } from './patient-login/patient-login.component';
 const routes: Routes = [
   {path: '', component: LandingPageComponent },
+  {path: 'contact', component: ContactComponent },
+  {path: 'about', component: AboutComponent },
+  {path: '/nurse', component: AboutComponent },
+  {path: '/patient', component: AboutComponent },
 ];
 
 @NgModule({
