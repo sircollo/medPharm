b348c44f58774c8be71bbf9f09735b7e8d95b918
Collins Sirwani <sircollo71@gmail.com>
2022-09-20 12:20:10 +0300

    Ft: adds routing to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { AboutComponent } from './aboutUs/about/about.component';
 import { NurseComponent } from './nurse/nurse.component';
 import { PatientLoginComponent } from './patient-login/patient-login.component';
 import { NurseDashboardComponent } from './nurse-dashboard/nurse-dashboard.component';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
 const routes: Routes = [
   {path: '', component: LandingPageComponent },
   {path: 'contact', component: ContactComponent },
   {path: 'about', component: AboutComponent },
-  {path: '/nurse', component: NurseComponent },
-  {path: '/patient', component: PatientLoginComponent },
-  {path: '/nurse-dashboard', component: NurseDashboardComponent },
+  {path: 'nurse', component: NurseComponent },
+  {path: 'patient', component: PatientLoginComponent },
+  {path: 'nurse-dashboard', component: NurseDashboardComponent },
+  {path: 'patient-dashboard', component: PatientDashboardComponent },
 ];
 
 @NgModule({
