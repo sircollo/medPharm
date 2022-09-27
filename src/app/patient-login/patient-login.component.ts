60dc1b0a91a051240ee34e7b6dbbbd6b8448f50f
Collins Sirwani <sircollo71@gmail.com>
2022-09-27 23:21:09 +0300

    Ft: implement login functionality endpoint

--- src/app/patient-login/patient-login.component.ts
+++ src/app/patient-login/patient-login.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-patient-login',
   templateUrl: './patient-login.component.html',
   styleUrls: ['./patient-login.component.css']
 })
 export class PatientLoginComponent implements OnInit {
-
-  constructor() { }
+  form:any = {
+    userId:null,
+    password:null,
+  }
+  userName!: null;
+  constructor(private authService:AuthService, private router:Router){ }
 
   ngOnInit(): void {
+  }
+  onSubmit():void{
+    const {userId, password} = this.form;
+    this.authService.login(userId,password).subscribe(
+      data => {
+        console.log(data);
+        console.log(data.data.roles)
+        if ( data.data.roles[0] === "Admin"){
+          this.router.navigate(["/admin-dashboard"]).then(() => {
+              window.location.reload();
+            });
+        }
+        else if( data.data.roles[0] === "Patient" ){
+          this.router.navigate(["/patient-dashboard"]).then(() => {
+            window.location.reload();
+          });
+        } 
+        else if( data.data.roles[0] === "Staff" ){
+          this.router.navigate(["/nurse-dashboard"]).then(() => {
+            window.location.reload();
+          });
+        } 
+      }
+    )
+
   }
 
 }
