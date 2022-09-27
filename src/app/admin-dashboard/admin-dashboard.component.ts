9a2c2ff8bbe51719fa51dd96459b82e19d77a127
Collins Sirwani <sircollo71@gmail.com>
2022-09-28 02:11:24 +0300

    Ft: add logout functionality and dynamic user naming

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TokenStorageService } from '../services/token-storage.service';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
-
-  constructor() { }
+  username!:string
+  constructor(private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
+    const user = this.tokenStorage.getUser()
+    this.username = user.data.userName
+    console.log(this.username)
+  }
+  logout(): void {
+    this.tokenStorage.signOut();
+    window.location.reload();
+  }
+  reloadPage(): void {
+    window.location.reload();
   }
 
 }
