f9d8dbfac8a82b35e0bda939c3e82a7c02fbd190
Collins Sirwani <sircollo71@gmail.com>
2022-09-28 03:31:45 +0300

    Ft: implemet error throw

--- src/app/account/medpharm/medpharm.component.ts
+++ src/app/account/medpharm/medpharm.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-medpharm',
   templateUrl: './medpharm.component.html',
@@ -15,12 +16,12 @@ export class MedpharmComponent implements OnInit {
     phoneNumber: null,
     password: null,
     password1: null,
+    gender:null,
   }
-  gender: any = [1, 2]
+  // gender: any = [1, 2]
   roleNames: any = [["Staff"], ["Patient"]]
-
-  
-  constructor(private http : HttpClient) { }
+  errorMessage = ''
+  constructor(private http : HttpClient, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -28,7 +29,12 @@ export class MedpharmComponent implements OnInit {
     this.http.post("http://fmkdev-001-site1.dtempurl.com/api/User/create",formData).subscribe((data) => {
       console.log(data)
       alert("Registration Successfull")
-      
-    })
+      this.router.navigate(["/login"]).then(() => {
+        window.location.reload();
+      });
+    },err => {
+      this.errorMessage = err.error.message;
+      alert(this.errorMessage)
+  })
   }
 }
