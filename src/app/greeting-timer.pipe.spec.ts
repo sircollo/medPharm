d2b98d1c56ddee65c57bf4ab52a776b2046d880a
Collins Sirwani <sircollo71@gmail.com>
2022-09-28 19:56:57 +0300

    Auto-generated commit [ci skip]

--- src/app/greeting-timer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GreetingTimerPipe } from './greeting-timer.pipe';
-
-describe('GreetingTimerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GreetingTimerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
